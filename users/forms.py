532c2e1b25c47f5ecd2a1076d992410247e7e188
Moyosore1 <moyosoreolumideobi@gmail.com>
2020-10-14 15:14:11 +0100

    a commit

---
+++ users/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        widgets = {
+        	'username': forms.TextInput(attrs={'class':'form-control', 'required':True}), 
+        	'email':forms.EmailInput(attrs={'class':'form-control', 'required':True}),
+        	'first_name':forms.TextInput(attrs={'class':'form-control', 'required':True}),
+        	'last_name':forms.TextInput(attrs={'class':'form-control', 'required':True}),
+        	'password1':forms.PasswordInput(attrs={'class':'form-control', 'required':True}),
+        	'password2':forms.PasswordInput(attrs={'class':'form-control', 'required':True}),
+
+        }
+
+
